21e7e4a4aa3451c3e3549c11837db29a2ae472d1
Daniel <Danielmikhh@gmail.com>
2024-07-25 19:41:44 +0200

    Добавленные две страницы: Login и Sing in

---
+++ Film lib/Models/RegisterViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Film_lib.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public required string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public required string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public required string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public required string ConfirmPassword { get; set; }
+
+        public void RememberMe(bool v)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public required string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public required string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public required bool RememberMe { get; set; }
+    }
+
+}
