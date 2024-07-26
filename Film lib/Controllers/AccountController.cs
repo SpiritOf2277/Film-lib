a6fbaab28ec4ca27c88b06b47d8ec9688ceb7b30
Daniel <Danielmikhh@gmail.com>
2024-07-26 09:27:56 +0200

    Исправлен баг с паролем

--- Film lib/Controllers/AccountController.cs
+++ Film lib/Controllers/AccountController.cs
@@ -29,18 +29,11 @@ namespace Film_lib.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterViewModel model)
         {
-            if (ModelState.IsValid)
-            {
-                bool userExists = _context.Users.Any(u => u.Username == model.Username || u.Email == model.Email);
-                if (userExists) {
-                    ModelState.AddModelError(string.Empty, "Username or Email already exists.");
-                    return View(model);
-                }
-
-                IdentityUser identityUser = new IdentityUser { UserName = model.Username, Email = model.Email };
-                IdentityResult result = await _userManager.CreateAsync(identityUser, model.Password);
+            if (ModelState.IsValid) {
+                var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded) {
-                    await _signInManager.SignInAsync(identityUser, isPersistent: false);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
                     return RedirectToAction("Index", "Home");
                 }
                 foreach (var error in result.Errors) {
